111f2e90d157057bf31171e451cec71a2b92b984
harry <harrisongitau3@gmail.com>
2021-02-27 15:50:46 +0300

    Fix password reset error

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app import db, login, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import jwt
+from time import time
 
 
 class User(UserMixin, db.Model):
@@ -18,6 +20,20 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
 @login.user_loader
