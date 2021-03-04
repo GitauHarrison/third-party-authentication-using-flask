3189133700488d15c0e21ccc151bd66918abba1c
harry <harrisongitau3@gmail.com>
2021-03-04 05:53:41 +0300

    Fix password reset error

--- app/models.py
+++ app/models.py
@@ -24,8 +24,8 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
-
+            app.config['SECRET_KEY'], algorithm='HS256')
+            
     @staticmethod
     def verify_reset_password_token(token):
         try:
