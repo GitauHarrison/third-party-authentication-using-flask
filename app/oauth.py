719ca4254be882ccacb06597637aaee701711203
harry <harrisongitau3@gmail.com>
2021-03-31 02:54:38 +0300

    Add oauth module

---
+++ app/oauth.py
@@ -0,0 +1,9 @@
+from app import app
+
+
+class OAuthSignIn(object):
+    providers = None
+
+    def __init__(self, provider_name):
+        self.provider_name = provider_name
+        credentials = app.config['OAUTH_CREDENTIALS'][provider_name]
