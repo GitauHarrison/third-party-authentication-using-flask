8a46324595efcb85015bfc186a28ebe887c13e36
harry <harrisongitau3@gmail.com>
2021-03-31 05:49:13 +0300

    Add callback route

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from app.models import User
 from app.email import send_password_reset_email
 from datetime import datetime
 from werkzeug.urls import url_parse
-from oauth import OAuthSignIn
+from app.oauth import OAuthSignIn
 
 
 @app.route('/')
@@ -94,7 +94,25 @@ def before_request():
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
-    if not current_user.is_anonymous():
+    if not current_user.is_anonymous:
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
     return oauth.authorize()
+
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed')
+        return redirect(url_for(index))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('index'))
