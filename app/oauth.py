b64e600067a08cc6b2fffc14657820be7b119d6c
harry <harrisongitau3@gmail.com>
2021-03-31 03:14:22 +0300

    Add abstraction layer on top of Rauth

--- app/oauth.py
+++ app/oauth.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import url_for
 
 
 class OAuthSignIn(object):
@@ -7,3 +8,34 @@ class OAuthSignIn(object):
     def __init__(self, provider_name):
         self.provider_name = provider_name
         credentials = app.config['OAUTH_CREDENTIALS'][provider_name]
+        self.consumer_id = credentials['id']
+        self.consumer_secret = credentials['secret']
+
+    def authorize(self):
+        pass
+
+    def callback(self):
+        pass
+
+    def get_callback_url(self):
+        return url_for('oauth_callback',
+                       provider=self.provider_name,
+                       _external=True
+                       )
+
+    @classmethod
+    def get_provider(self, provider_name):
+        if self.providers is None:
+            self.providers = {}
+            for provider_class in self.__subclasses__():
+                provider = provider_class()
+                self.providers[provider.provider_name] = provider
+        return self.providers[provider_name]
+
+
+class FacebookSignIn(OAuthSignIn):
+    pass
+
+
+class TwitterSignIn(OAuthSignIn):
+    pass
