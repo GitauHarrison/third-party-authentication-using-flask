706a27a06146c861f352cc7725605821ab5bef5c
harry <harrisongitau3@gmail.com>
2021-03-31 09:41:22 +0300

    Add sslmode to heroku db

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(basedir, '.env')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
